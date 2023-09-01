6d0d79a691dc911bee1313a96244dbd284094e3b
Bakate <bakateba@gmail.com>
2023-09-01 23:26:36 +0200

    feat(navbar): with the store-switcher, user can change or create an ew store

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,31 @@
-"use client";
-
 import MainNav from "@/components/main-nav";
 import { ModeToggle } from "@/components/mode-toggle";
-import { UserButton } from "@clerk/nextjs";
+import StoreSwitcher from "@/components/store-switcher";
+import prismadb from "@/lib/prismadb";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className="border-b">
       <div className="flex items-center  w-full h-16 px-4">
-        <div>This will be a store switcher button</div>
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-2" />
         <div className="ml-auto flex items-center space-x-4">
-          <UserButton afterSignOutUrl="/" />
           <ModeToggle />
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
