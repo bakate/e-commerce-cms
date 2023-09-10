28e61491455d4534c0d531045d0d027e4af04c26
Bakate <bakateba@gmail.com>
2023-09-10 13:37:56 +0200

    feat(checkout): add stripe to handle payments

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,18 @@
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
 const StorePage = () => {
   return (
-    <div>
-      <h1>Store Page</h1>
+    <div className="flex-col">
+      <div className="flex-1 p-6 pt-8 space-y-4">
+        <Heading title="Dashboard" description="overview of your store" />
+        <Separator />
+        <div className="grid gap-4 grid-cols-3">
+          <p>hello</p>
+          <p>hello</p>
+          <p>hello</p>
+        </div>
+      </div>
     </div>
   );
 };
